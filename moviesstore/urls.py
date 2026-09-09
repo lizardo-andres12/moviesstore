d4c32023a4c454678d68861fde182055c47b6134
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-09-09 00:27:34 -0400

    Add homepage and index.html

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+'''
+This file is the main URL router for the entire project. Starts search here and falls
+back to apps in case a pattern is not matched form here.
+'''
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
