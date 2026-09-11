7c264861e949098b0af0fdf751845ff81a78cc54
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-09-11 17:42:50 -0400

    Implement user account management

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
