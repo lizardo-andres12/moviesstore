028d28d95edda873174d1a91dca71f3aacd70406
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-09-10 21:39:40 -0400

    Beautify admin panel for Movie model

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Movie
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
